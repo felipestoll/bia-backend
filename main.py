a66039fda2fc73aac7dcfeb53fd5287b58830578
felipestoll <57003112+felipestoll@users.noreply.github.com>
2025-11-16 22:01:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import os # <--- ADICIONE ESTA LINHA
 from fastapi import FastAPI, Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
