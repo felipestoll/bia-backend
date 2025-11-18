94484746e76cee91f618d59d7f2668b6852144ec
felipestoll <57003112+felipestoll@users.noreply.github.com>
2025-11-18 13:10:12 -0300

    Update firebase_admin_config.py

--- firebase_admin_config.py
+++ firebase_admin_config.py
@@ -13,13 +13,11 @@ try:
             cred = credentials.Certificate(cred_json)
         else:
             # 2. Fallback para desenvolvimento local:
-            # Este caminho só é usado se você rodar localmente e a variável não estiver definida.
             cred = credentials.Certificate("serviceAccountKey.json")
             
         firebase_admin.initialize_app(cred)
 except Exception as e:
     # Lança um erro robusto se a conexão com o Firebase falhar
-    # Isso fará com que o Render mostre o traceback completo, em vez de Bad Gateway
     raise RuntimeError(f"Erro Crítico ao inicializar o Firebase: {e}. Verifique FIREBASE_CREDENTIALS.")
     
 # Instância do Firestore para uso global
